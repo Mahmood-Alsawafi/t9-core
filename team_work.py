4fd409e971cf7ca670e26756090456d880f76502
unknown <ahmedalnaimee44@gmail.com>
2025-05-18 15:28:03 +0400

    added random and abs

--- team_work.py
+++ team_work.py
@@ -1,3 +1,6 @@
+import random 
+
+
 num_1 = int(input("Enter the first number:"))
 num_2 = int(input("Enter the second number:"))
 opreation = input("Enter one of + - * ** / // mod and % : ")
@@ -16,6 +19,11 @@ elif opreation=="//":
      result = num_1 // num_2
 elif opreation=="mod":
      result = num_1 % num_2
+elif opreation == "random":
+    result = random.randrange(num_1, num_2)
+elif opreation == "abs":
+    result = abs(num_1), abs(num_2)
+
 
 else:
    print("opreation not supported")
