9c5f1478d411168f74657915423a73b1fa5fc48f
Mahmood <msssals54321@gmail.com>
2025-05-18 15:48:56 +0400

    added the function chouse

--- team_work.py
+++ team_work.py
@@ -3,7 +3,7 @@ import random
 
 num_1 = int(input("Enter the first number:"))
 num_2 = int(input("Enter the second number:"))
-opreation = input("Enter one of + - * ** / // mod and % : ")
+opreation = input("Enter one of + - * ** / // mod ranmom abs & | ~ ^ %  : ")
 
 if opreation == "+":
      result = num_1 + num_2
