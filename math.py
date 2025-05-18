37ddf2c4fce249be02a14fcacb523bd3604b9f98
Mahmood <msssals54321@gmail.com>
2025-05-18 15:01:12 +0400

    start the code

---
+++ math.py
@@ -0,0 +1,9 @@
+num_1 = int(input("Enter the first number:"))
+num_2 = int(input("Enter the second number:"))
+opreation = input("Enter one of + - * ** / // mod and % : ")
+
+if opreation == "+":
+     result = num_1 + num_2
+
+print("Result is: ", result)
+print("End of Script")
